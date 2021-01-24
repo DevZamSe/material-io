05de91e9fbe42a9cd48c165e48c695aad63fe5e8
devzamse <nilovila@e-quipu.pe>
2021-01-23 21:38:02 -0500

    salvando vidas

---
+++ app/src/main/java/com/gracia/login_java/Splash.java
@@ -0,0 +1,28 @@
+package com.gracia.login_java;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        init();
+    }
+
+    public void init(){
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(Splash.this, Login.class);
+                startActivity(i);
+            }
+        }, 500);
+    }
+}
